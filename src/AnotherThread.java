02be20fbde19576650f89c52c2d50603423f8649
gsylverman <gsylverman@gmail.com>
2020-09-24 15:54:14 +0300

    timeout on join thread

--- src/AnotherThread.java
+++ src/AnotherThread.java
@@ -3,12 +3,12 @@ public class AnotherThread extends Thread {
     public void run() {
         System.out.println(ThreadColor.ANSI_GREEN + "Hello from: " + currentThread().getName());
         try {
-            Thread.sleep(3000);
+            Thread.sleep(6000);
         } catch (InterruptedException e) {
             System.out.println("Another thread wake me up" + currentThread().getName());
             return;
         }
-        System.out.println("3 sec have passed, I am awake: " + currentThread().getName());
+        System.out.println("6 sec have passed, I am awake: " + currentThread().getName());
 
     }
 }
--- src/Threads.java
+++ src/Threads.java
@@ -15,13 +15,15 @@ public class Threads {
         System.out.println(ThreadColor.ANSI_BLUE + "Hello again from the main thread");
 //        Thread someClassThread = new Thread(new SomeClass());
         Thread someClassThread = new Thread(() -> {
-            System.out.println("Override");
             try {
-                anotherThread.join(); //wait for another tread to finish before starting
-                System.out.println("another thread terminated so I am running again");
+//                anotherThread.join(); //wait for another tread to finish before starting
+                anotherThread.join(2000); //wait for another tread 2 sec to finish before starting
+//                System.out.println("another thread terminated so I am running again");
+                System.out.println("Waited 2 sec for another thread, then continue");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            System.out.println("Waited for 2 sec or another thread to finish");
         });
         someClassThread.start();
 
