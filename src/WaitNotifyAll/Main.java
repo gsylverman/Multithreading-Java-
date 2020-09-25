b916a59dddea20b073cb3a81e933617232bfd22d
gsylverman <gsylverman@gmail.com>
2020-09-25 18:39:33 +0300

    waitNotifyAll

---
+++ src/WaitNotifyAll/Main.java
@@ -0,0 +1,99 @@
+package WaitNotifyAll;
+
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+    }
+}
+
+class Message {
+    private String message;
+    private boolean empty = true;
+
+    public synchronized String read() {
+        while (empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+        empty = true;
+        notifyAll();
+        return message;
+    }
+
+    public synchronized void write(String message) {
+        while (!empty) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+
+        }
+        empty = false;
+        this.message = message;
+        notifyAll();
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        String[] messages = {
+                "Effects present letters inquiry no an removed or friends",
+                "Desire behind latter me though in",
+                "Supposing shameless am he engrossed up additions",
+                "My possible peculiar together to.",
+                "Desire so better am cannot he up before points.",
+                "Remember mistaken opinions it pleasure of debating.",
+                "Court front maids forty if aware their at. Chicken use are pressed removed."
+        };
+
+        Random random = new Random();
+
+        for (int i = 0; i < messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    public void run() {
+        Random random = new Random();
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished");
+             latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+
+            }
+        }
+    }
+}
+
